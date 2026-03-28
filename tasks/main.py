393a4d5a01d53ca17b5071753d64308a60d4e8f4
amarqusee <169078599+amarqusee@users.noreply.github.com>
2026-03-27 21:33:33 -0400

    Implemented all three of extract, prepare, load and deployed

--- tasks/main.py
+++ tasks/main.py
@@ -92,10 +92,132 @@ def prepare_phl_opa_properties(request):
     print(f'Processed data into {prepared_filename}')
 
     # Upload the prepared data to the bucket
-    prepared_blobname = 'tables/phl_opa_properties/phl_opa_properties.jsonl'
+    prepared_blobname = 'opa_properties/data.jsonl'
     prepared_bucket = storage_client.bucket(prepared_bucket_name)
     blob = prepared_bucket.blob(prepared_blobname)
     blob.upload_from_filename(prepared_filename)
     print(f'Uploaded to {prepared_blobname}')
 
     return f'Processed data into {prepared_filename} and uploaded to gs://{prepared_bucket_name}/{prepared_blobname}'
+
+#Load
+
+from dotenv import load_dotenv
+load_dotenv()
+
+import os
+import functions_framework
+from google.cloud import bigquery
+
+SOURCE_DATASET = os.getenv('SOURCE_DATASET', 'source')
+CORE_DATASET = os.getenv('CORE_DATASET', 'core')
+PREPARED_DATA_BUCKET = os.getenv('PREPARED_DATA_BUCKET', 'musa5090s26-team4-prepared_data')
+
+
+@functions_framework.http
+def load_opa_properties(request):
+    bigquery_client = bigquery.Client()
+
+    # Step 1: Create/update the external table in source dataset
+    source_query = f'''
+        CREATE OR REPLACE EXTERNAL TABLE `{SOURCE_DATASET}.opa_properties` (
+            `objectid` STRING,
+            `assessment_date` STRING,
+            `basements` STRING,
+            `beginning_point` STRING,
+            `book_and_page` STRING,
+            `building_code` STRING,
+            `building_code_description` STRING,
+            `category_code` STRING,
+            `category_code_description` STRING,
+            `census_tract` STRING,
+            `central_air` STRING,
+            `cross_reference` STRING,
+            `date_exterior_condition` STRING,
+            `depth` STRING,
+            `exempt_building` STRING,
+            `exempt_land` STRING,
+            `exterior_condition` STRING,
+            `fireplaces` STRING,
+            `frontage` STRING,
+            `fuel` STRING,
+            `garage_spaces` STRING,
+            `garage_type` STRING,
+            `general_construction` STRING,
+            `geographic_ward` STRING,
+            `homestead_exemption` STRING,
+            `house_extension` STRING,
+            `house_number` STRING,
+            `interior_condition` STRING,
+            `location` STRING,
+            `mailing_address_1` STRING,
+            `mailing_address_2` STRING,
+            `mailing_care_of` STRING,
+            `mailing_city_state` STRING,
+            `mailing_street` STRING,
+            `mailing_zip` STRING,
+            `market_value` STRING,
+            `market_value_date` STRING,
+            `number_of_bathrooms` STRING,
+            `number_of_bedrooms` STRING,
+            `number_of_rooms` STRING,
+            `number_stories` STRING,
+            `off_street_open` STRING,
+            `other_building` STRING,
+            `owner_1` STRING,
+            `owner_2` STRING,
+            `parcel_number` STRING,
+            `parcel_shape` STRING,
+            `quality_grade` STRING,
+            `recording_date` STRING,
+            `registry_number` STRING,
+            `sale_date` STRING,
+            `sale_price` STRING,
+            `separate_utilities` STRING,
+            `sewer` STRING,
+            `site_type` STRING,
+            `state_code` STRING,
+            `street_code` STRING,
+            `street_designation` STRING,
+            `street_direction` STRING,
+            `street_name` STRING,
+            `suffix` STRING,
+            `taxable_building` STRING,
+            `taxable_land` STRING,
+            `topography` STRING,
+            `total_area` STRING,
+            `total_livable_area` STRING,
+            `type_heater` STRING,
+            `unfinished` STRING,
+            `unit` STRING,
+            `utility` STRING,
+            `view_type` STRING,
+            `year_built` STRING,
+            `year_built_estimate` STRING,
+            `zip_code` STRING,
+            `zoning` STRING,
+            `pin` STRING,
+            `building_code_new` STRING,
+            `building_code_description_new` STRING,
+            `geog` STRING
+        )
+        OPTIONS (
+            format = 'JSON',
+            uris = ['gs://{PREPARED_DATA_BUCKET}/opa_properties/data.jsonl']
+        )
+    '''
+    bigquery_client.query_and_wait(source_query)
+    print(f'Created/updated {SOURCE_DATASET}.opa_properties')
+
+    # Step 2: Create/update the internal core table with property_id
+    core_query = f'''
+        CREATE OR REPLACE TABLE `{CORE_DATASET}.opa_properties` AS
+        SELECT
+            parcel_number AS property_id,
+            *
+        FROM `{SOURCE_DATASET}.opa_properties`
+    '''
+    bigquery_client.query_and_wait(core_query)
+    print(f'Created/updated {CORE_DATASET}.opa_properties')
+
+    return f'Successfully loaded opa_properties into {SOURCE_DATASET} and {CORE_DATASET} datasets'
